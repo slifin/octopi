e8cb879f2a13fafd425240ce7ebd8184a95c1f24
Adrian Smith <sfyire@gmail.com>
2019-04-26 20:47:46 +0100

    Playing with honeySQL

--- src/octopi.clj
+++ src/octopi.clj
@@ -1,13 +1,27 @@
-(ns octopi)
+(ns octopi
+  (:require
+      [honeysql.core :as sql]
+      [honeysql.helpers :refer :all :as helpers]))
 
 (defn -main [& args]
-  (println "hey")
-  (println args))
+  (println "hey"))
 
-(defn hello [name]
-  (str "hello" " " name))
+(defn query-map []
+ {:select [:uid :name]
+  :from [:users]
+  :where [:= :users.name "Barry"]})
+
+(defn by-id []
+  (merge-where (query-map) [:= :users.uid 5]))
 
 
+(defn sub-query []
+  {:select [:*]
+   :from [(by-id) "sub"]})
+
+
+(defn union-query []
+  {:union-all [(query-map) (query-map)]})
 
 
 
