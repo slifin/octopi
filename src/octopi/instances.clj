7bd0982df94ade8159d1db9e99914d521658c0b3
Adrian Smith <sfyire@gmail.com>
2019-04-26 23:05:54 +0100

    converting some SQL to honeySQL

--- src/octopi/instances.clj
+++ src/octopi/instances.clj
@@ -1,7 +1,31 @@
 (ns octopi.instances
   "Functions to define instance queries"
   (:require
-    [honeysql.core :as sql]
-    [honeysql.helpers :refer :all :as helpers]))
+      [honeysql.core :as sql]))
 
+;; Columns
+(defn ids[]
+  [:nid :vid])
+
+;; Tables
+(defn table []
+  [:appointment_instances])
+
+;; Conditions
+(defn started? [start-date]
+  [:>= :instance_date start-date])
+
+(defn ended? [end-date]
+  [:<= :instance_date end-date])
+
+(defn client? [uid]
+  [:= :uid uid])
+
+(comment
+  (sql/format {:select (ids)
+               :from (table)
+               :where [:and
+                        (started? 1556313105)
+                        (ended? 1556319600)
+                        (client? 23)]}))
 
