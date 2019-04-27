975861077b8a68d77dcf108a05b77c946a9c526b
Adrian Smith <sfyire@gmail.com>
2019-04-27 12:21:24 +0100

    Preparing for lucinia

--- src/octopi/core.clj
+++ src/octopi/core.clj
@@ -1,26 +1,11 @@
 (ns octopi.core
   (:require
-    [honeysql.core :as sql]
-    [honeysql.helpers :refer :all :as helpers]))
+    [clojure.java.io :as io]))
 
 (defn -main [& args]
   (println "hey"))
 
-(defn query-map []
- {:select [:uid :name]
-  :from [:users]
-  :where [:= :users.name "Barry"]})
 
-(defn by-id []
-  (merge-where (query-map) [:= :users.uid 5]))
-
-(defn sub-query []
-  {:select [:*]
-   :from [(by-id) "sub"]})
-
-
-(defn union-query []
-  {:union-all [(query-map) (query-map)]})
 
 
 
---
+++ src/octopi/schema.clj
@@ -0,0 +1,16 @@
+(ns octopi.schema
+  (:require [clojure.java.io :as io]
+            [com.walmartlabs.lacinia.util :as util]
+            [com.walmartlabs.lacinia.schema :as schema]
+            [clojure.edn :as edn]))
+
+
+(defn )
+
+(defn load-schema []
+  (-> (io/resource "schema.edn")
+      slurp
+      edn/read-string
+      (util/attach-resolvers (resolver-map))
+      schema/compile))
+
